a93824eac25b23b8a4c61bc7c27eb7ff9c4bcddb
Cpasgrave <33382549+Cpasgrave@users.noreply.github.com>
2018-07-17 00:40:18 +0200

    Create Lipogram_Hello_World!.py

---
+++ Lipogram_Hello_World!.py
@@ -0,0 +1,11 @@
+# https://www.sololearn.com/Discuss/1018458/challange-write-a-python-program-with-output-hello-world-with-a-little-handicap
+# Challenge proposed by Oma Falk
+
+'''
+Write a python program with output "Hello world" with a little handicap...
+....  the character "p"  is forbidden in the whole coding exception is the keyword import,
+no print, no map,....
+have fun
+'''
+
+exec(''.join(chr(i)for i in[112,114,105,110,116,40,39,72,101,108,108,111,32,87,111,114,108,100,33,39,41]))
