c9292d3705921b022ed9d06313853c2bf1bb0b50
Cpasgrave <33382549+Cpasgrave@users.noreply.github.com>
2018-07-17 12:40:38 +0200

    Delete Even_Odd_3.py

--- Even_Odd_3.py
+++
@@ -1,9 +0,0 @@
-a = 3477
-
-c = int(int(str(a/2)[-1])/5)*'odd'+int(int(str(a/2-0.5)[-1])/5)*'even'
-print(a,'is',c)
-
-a = 3478
-
-c = int(int(str(a/2)[-1])/5)*'odd'+int(int(str(a/2-0.5)[-1])/5)*'even'
-print(a,'is',c)
