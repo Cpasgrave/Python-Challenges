9ae1cedc5b51440d422a11aecf27667b4acc38f4
Cpasgrave <33382549+Cpasgrave@users.noreply.github.com>
2018-07-17 12:28:01 +0200

    Create Even_Odd_3.py

---
+++ Even_Odd_3.py
@@ -0,0 +1,9 @@
+a = 3477
+
+c = int(int(str(a/2)[-1])/5)*'odd'+int(int(str(a/2-0.5)[-1])/5)*'even'
+print(a,'is',c)
+
+a = 3478
+
+c = int(int(str(a/2)[-1])/5)*'odd'+int(int(str(a/2-0.5)[-1])/5)*'even'
+print(a,'is',c)
