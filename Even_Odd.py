024af879a43b9b13714f462b2baab7f9a7ba5e65
Cpasgrave <33382549+Cpasgrave@users.noreply.github.com>
2018-07-17 12:25:29 +0200

    Create Even_Odd.py

---
+++ Even_Odd.py
@@ -0,0 +1,13 @@
+# Challenge by Fabio
+# https://www.sololearn.com/Discuss/1156970/challenge-even-or-odd-number
+'''
+Challenge - Even or Odd number.
+Make a program that takes a given number and let know to the user whether that number is even or not, but you can't use any condition nor loop statement like if-else, switch, ternary, while, do while, for, for-each. 
+All languages are welcome. 
+Any question or suggestion is welcome too. 
+Have fun!
+'''
+
+a = 19254157
+
+print(a,'is',['even','odd'][a%2])
